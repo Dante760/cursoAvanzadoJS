4691e3c9075da5db2125edf27952a8d0d80f3082
Dante760 <danflixx760yo@gmail.com>
2025-07-06 20:47:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+//Pegar nav al principio + animacion
+function mostrar() {
+    if(window.scrollY > window.innerHeight*0.15){
+        document.querySelector("nav").classList.add("enScroll");
+    }
+    else{
+        document.querySelector("nav").classList.remove("enScroll");
+    }
+
+    if(window.scrollY > window.innerHeight*0.75){
+        document.querySelector(".Tarjeta1").classList.add("aparecerDerecha");
+        document.querySelector(".Tarjeta2").classList.add("aparecerIzquierda");
+    }
+}
+
